16efa14e88596580462ce13b29b957b842fe208d
NikkeKatski <77799777+NikkeKatski@users.noreply.github.com>
2025-01-08 17:44:35 -0600

    unfludd

--- include/BetterSMS/fludd.hxx
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SMS/Player/NozzleBase.hxx>
-
-namespace BetterSMS {
-    namespace Fludd {
-        typedef TNozzleBase *(*NozzleInitializer)(TWaterGun *watergun);
-        bool registerNozzle(const char *name, NozzleInitializer initFn, u32 numStreams, u32 jointEmitters[4]);
-    }  // namespace Fludd
-};     // namespace BetterSMS
